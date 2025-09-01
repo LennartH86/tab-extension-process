45496950b4ee6e9701d0ee2a9e4fa66c97ecc892
LennartH86 <lennart.heuckendorf@gmail.com>
2025-09-01 13:55:50 +0200

    Migrated to Node.js server for static hosting on Heroku

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Serve static files from the root of your project
+app.use(express.static(path.join(__dirname)));
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
