010b5ba2b7b05aaaafb07cd893eab67a9e2c6550
LennartH86 <lennart.heuckendorf@gmail.com>
2025-09-01 14:15:38 +0200

    Adjustments for external extension.js

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Serve static files from the root of your project
+// Heroku will serve the static files from the root of the "Annotation" folder
 app.use(express.static(path.join(__dirname)));
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });
